a1fdf1de240f5d2107974fc98a4d21dd84c7efdc
Jordan McKenna <jordanmckenna@JORDANs-MacBook-Pro.local>
2021-09-25 16:58:33 +0100

    first project commit and created basic folders and files with boiler plate info

---
+++ models/fixture.py
@@ -0,0 +1,8 @@
+class Location:
+
+    def __init__(self, home_team, away_team, result, points, id = None):
+      self.home_team = home_team
+      self.away_team = away_team
+      self.result = result
+      self.points = points
+      self.id = id
---
+++ models/team.py
@@ -0,0 +1,6 @@
+class Team:
+
+    def __init__(self, name, points, id = None):
+      self.name = name
+      self.points = points
+      self.id = id
